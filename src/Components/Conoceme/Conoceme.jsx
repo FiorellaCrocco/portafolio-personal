659673dc3512354f161a73346589094cef8dfabc
FiorellaCrocco <fiorellacrocco@gmail.com>
2024-05-13 23:24:53 -0300

    conoceme mejorado

--- src/Components/Conoceme/Conoceme.jsx
+++ src/Components/Conoceme/Conoceme.jsx
@@ -11,14 +11,25 @@ const Conoceme = () => {
       <div className="text-container">
         <h2>¡Hola! Soy Fiorella Crocco</h2>
         <p>
-          Soy una apasionada del mundo de la informática y emocionada de seguir
-          capacitándome en infraestructura en la nube. Me gradué en Artes
-          Gráficas y Desarrollo de Software, actualmente me encuentro
-          especializándome en backend. Con una sólida formación en Desarrollo de
-          Software, adquirida a través de la carrera en Digital House y
-          respaldada por la Universidad Tecnológica del Uruguay (UTEC), poseo
-          una amplia gama de habilidades blandas y técnicas con experiencia
-          práctica en el desarrollo de aplicaciones.
+          Una apasionada del mundo de la informática, consciente de la
+          importancia de la capacitación continua, con especial interés en
+          infraestructura en la nube. Me gradué en Artes Gráficas y Desarrollo
+          de Software, actualmente me encuentro especializándome en backend. Mi
+          formación en Desarrollo de Software es sólida dado los conocimientos
+          adquiridos a través de la carrera Certified Tech Developer creada por
+          Globant y Mercado Libre junto con la Universidad Tecnológica del
+          Uruguay (UTEC), quien la respalda. Además de mis conocimientos previos
+          como Full Stack Developer en 4Geeks Academy. Poseo una amplia gama de
+          habilidades blandas y técnicas con experiencia práctica en el
+          desarrollo de diversas aplicaciones junto con su respectivo
+          despliegue. Gracias a mis diversos estudios tengo un panorama muy
+          amplio en diferentes áreas, lo que me brinda una visión particular de
+          las necesidades de los proyectos desde diversos puntos, como ser,
+          diseño gráfico, experiencia de usuario, planificación y gestión
+          estratégica (especialmente en metodologías ágiles dado que desde que
+          inicie mi intensa formación en tecnología hace ya 4 años siempre he
+          desempeñado el rol de Scrum Master) y por supuesto en desarollo,
+          testing e infraestructura, junto con el desarrollo de mis habilidades blandas.
         </p>
       </div>
       <div className="conocimientos">
@@ -79,7 +90,7 @@ const Conoceme = () => {
           </ul>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
